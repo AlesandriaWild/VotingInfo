15b594634d5eb4ff16037367b8cd8387d94c8745
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-10 14:09:39 -0700

    Working if, else statement. Css added and finialized. Code currently running as expected.
    Co-authored-by: Noah Cowan <nsncowan@gmail.com>

--- js/script.js
+++ js/script.js
@@ -7,15 +7,20 @@ form.onsubmit = function(e) {
 
 
   let over18 = document.getElementById("over_18");
-  let under18 = document.getElementById("under_18");
   over18.setAttribute("class", "hidden");
+  let under18 = document.getElementById("under_18");
   under18.setAttribute("class", "hidden");
 
   const age = parseInt(document.querySelector("input#age").value);
 
-  if (age >= 18)
+  if (age >= 18) {
+    over18.removeAttribute("class");
+  } 
 
+    else {
 
-}
-  
-}
+     under18.removeAttribute("class");
+
+    }
+  };
+};
